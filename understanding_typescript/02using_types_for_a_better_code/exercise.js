692e1e96f1f0f418f52f955a9706c0a904cd5798
kevin sun <qpjoy2018@gmail.com>
2019-03-04 22:14:54 +0800

    + understanding

---
+++ understanding_typescript/02using_types_for_a_better_code/exercise.js
@@ -0,0 +1,19 @@
+let bankAccount = {
+    money: 2000,
+    deposit(value) {
+        this.money += value;
+    }
+};
+
+let myself = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
+
+.log(myself);
+
